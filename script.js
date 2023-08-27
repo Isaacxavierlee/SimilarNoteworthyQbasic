44d39860e174f350f7fc06c61965d38b617afb4e
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-08-27 15:17:11 +0000

    x

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
       imageContainer.classList.toggle('zoomed');
     });
 
-    // Mouse-based interaction
     container.addEventListener('mouseenter', function() {
       const imageContainer = this;
       imageContainer.classList.add('zoomed');
@@ -25,29 +24,6 @@ document.addEventListener("DOMContentLoaded", function() {
     container.addEventListener('mouseleave', function() {
       const imageContainer = this;
       imageContainer.classList.remove('zoomed');
-    });
-
-    // Touch-based interaction
-    let initialTouchPosition = null;
-    container.addEventListener('touchstart', function(event) {
-      initialTouchPosition = event.touches[0].clientX;
-    });
-
-    container.addEventListener('touchmove', function(event) {
-      if (!initialTouchPosition) return;
-
-      const currentTouchPosition = event.touches[0].clientX;
-      const touchDelta = currentTouchPosition - initialTouchPosition;
-
-      if (Math.abs(touchDelta) > 30) {
-        const imageContainer = this;
-        imageContainer.classList.toggle('zoomed');
-        initialTouchPosition = null;
-      }
-    });
-
-    container.addEventListener('touchend', function() {
-      initialTouchPosition = null;
     });
   });
 });
