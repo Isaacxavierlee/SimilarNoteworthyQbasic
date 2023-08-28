d4dad057f8394730c0f795c9cd06e1b219b78988
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-08-28 09:12:57 +0000

    d

--- about.js
+++ about.js
@@ -7,6 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   themeToggle.addEventListener('click', toggleTheme);
 
+// Get all timeline title elements
+const timelineTitles = document.querySelectorAll('.timeline-title');
+
+// Attach click event listeners to each title
+timelineTitles.forEach(title => {
+  title.addEventListener('click', () => {
+    // Toggle the display of the content when clicked
+    const content = title.nextElementSibling;
+    content.style.display = content.style.display === 'none' ? 'block' : 'none';
+  });
+});
 
 
 
