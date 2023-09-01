fbd7d76a5d57dba4ce821eae836963b7a430edb1
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-09-01 03:51:28 +0000

    control active button

--- about.js
+++ about.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const themeToggle = document.getElementById("theme-toggle");
   const body = document.body;
-
+  
   themeToggle.addEventListener("click", function() {
     body.classList.toggle("light-mode");
   });
@@ -18,11 +18,23 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // Get all control icons
-  const controlsContainer = document.querySelector(".controls-container");
+  const controlIcons = document.querySelectorAll('.control-icon');
   const controls = document.querySelector(".controls");
   const themeBtn = document.querySelector(".theme-btn");
 
   themeBtn.addEventListener("click", function() {
     controls.classList.toggle("hidden");
+  });
+
+  controlIcons.forEach(icon => {
+    const currentPath = window.location.pathname;
+    const iconLink = icon.parentElement.getAttribute("href");
+
+    if (currentPath.includes(iconLink)) {
+      controlIcons.forEach(otherIcon => {
+        otherIcon.parentElement.classList.remove('active-btn');
+      });
+      icon.parentElement.classList.add('active-btn');
+    }
   });
 });
